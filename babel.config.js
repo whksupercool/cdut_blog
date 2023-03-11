74a550dadd6bd8f9617845da0ec00382eadcd58d
whksupercool <1225611688@qq.com>
2023-03-11 19:46:33 +0800

    项目初始化-引入vant组件库

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  // vant按需引入配置
+  plugins: [
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }, 'vant']
   ]
 }
